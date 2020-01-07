35949f740428a853c82da5397316f86bfcc28e67
Rajesh P <rajesh.pethe@gmail.com>
2020-01-07 17:33:31 +0530

    Find the sum of all multiple of three or five less than the given number.

---
+++ multiples-of-3-and-5.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+"""
+Multiples of 3 and 5
+   
+Problem 1
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+Answer:
+	233168
+
+"""
+
+
+def sum_of_multiples_of_three_and_five(number): 
+    answer = 0 
+    three = 3 
+    while three < number:
+        answer += three
+        for i in range(1, 3):
+            if (three - i) % 5 == 0:
+                answer += three - i
+                break
+        three += 3
+    return answer
+
+
+assert(sum_of_multiples_of_three_and_five(1000) == 233168)
+
